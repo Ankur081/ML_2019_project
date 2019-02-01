8d687925f04e48cb274c054bca9a8bc603a99bd4
Ankur jain <45650353+Ankur081@users.noreply.github.com>
2019-02-01 09:51:29 +0530

    cpu_processing_ 28-01-2019

---
+++ cpu_processing.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -
+import time
+import psutil,matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use('TkAgg')
+fig = plt.figure()
+ax = fig.add_subplot(111)
+fig.show()
+
+i = 0
+x, y = [], []
+while True:
+    x.append(i)
+    y.append(psutil.cpu_percent())
+    print(len(y))
+    if len(y) >=20:
+        y.pop(0)
+        x.pop(0)
+    ax.plot(x, y, color='b')  
+    fig.canvas.draw()
+    
+    ax.set_xlim(left=max(0, i-30), right=i+30)
+    
+    time.sleep(0.1)
+    i += 1
+
